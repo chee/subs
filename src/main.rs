54e3ca9ccdf23b28ff4d48c9eac3e541e674d858
chee <chee@snoot.club>
2019-08-14 13:03:35 +0100

    add program arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate getopts;
 extern crate regex;
 
+use getopts::Options;
 use regex::Regex;
 use std::fs;
 use std::io::prelude::*;
@@ -66,16 +68,47 @@ fn parse_msg(msg: &str) -> Option<Msg> {
     })?
 }
 
+fn print_usage(dollar0: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] PROGRAM [root_dir]", dollar0);
+    println!("{}", opts.usage(&brief));
+    println!("PROGRAM will be run in parallel in every subdirectory (SUB), as SUB's owner.");
+    println!("A placeholder '{{}}' is available to PROGRAM, it will be replaced with SUB.");
+    println!("A socket will be created.");
+    println!("Sending the socket a message like 'restart SUB' will restart that SUB's process.")
+}
+
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = std::env::args().collect();
-    let program = if let Some(program) = args.get(1) {
-        program
-    } else {
-        panic!("Usage: {} <program> [root_dir]");
+    let dollar0 = &args[0];
+    let mut options = Options::new();
+    options.optopt(
+        "s",
+        "socket",
+        "set the socket path. [default: subsocket]",
+        "NAME",
+    );
+    options.optflag("S", "no-socket", "do not create a socket");
+    options.optflag("h", "help", "get help");
+
+    let matches = match options.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
     };
 
+    let sock_path = matches.opt_str("s").unwrap_or("subsocket".to_string());
+
+    let program = matches.free.get(0);
     let default_root_dir = ".".to_string();
-    let root_dir = args.get(2).unwrap_or(&default_root_dir);
+    let root_dir = matches.free.get(1).unwrap_or(&default_root_dir);
+
+    let should_show_help = matches.opt_present("h") || program.is_none();
+
+    if should_show_help {
+        print_usage(&dollar0, options);
+        std::process::exit(1);
+    }
+
+    let program = program.unwrap();
 
     let subdirectories = fs::read_dir(root_dir)?;
 
@@ -96,34 +129,42 @@ fn main() -> Result<(), std::io::Error> {
         }
     }
 
-    let sock_path = "./subsocket";
-    fs::remove_file(sock_path).unwrap_or_default();
-    let sock = UnixListener::bind(sock_path)?;
-    for stream in sock.incoming() {
-        let mut buf = vec![];
-        let count = stream?.read_to_end(&mut buf)?;
-        println!("count: {:?}", count);
-        let string = String::from_utf8(buf).unwrap();
-        let msg = parse_msg(string.as_str());
-        match msg {
-            Some(msg) => {
-                let sub = processes.get_mut(msg.sub);
-                match sub {
-                    Some(sub) => {
-                        if msg.command == "restart" {
-                            sub.start(program)?;
-                        } else {
-                            println!("recieved unusual command: {}", msg.command)
-                        }
-                    }
-                    None => println!("recieved unusual person: {}", msg.sub),
-                }
+    if matches.opt_present("S") {
+        for sub in processes.values_mut() {
+            if let Some(process) = &mut sub.process {
+                process.wait().unwrap();
             }
-            None => println!(
-                "recieved unusual message. message should be <command> <sub>. got: {}",
-                string
-            ),
         }
+    } else {
+        fs::remove_file(&sock_path).unwrap_or_default();
+        let sock = UnixListener::bind(&sock_path)?;
+        for stream in sock.incoming() {
+            let mut buf = vec![];
+            stream?.read_to_end(&mut buf)?;
+            let string = String::from_utf8(buf).unwrap();
+            let msg = parse_msg(string.as_str());
+            match msg {
+                Some(msg) => {
+                    let sub = processes.get_mut(msg.sub);
+                    match sub {
+                        Some(sub) => {
+                            if msg.command == "restart" {
+                                sub.start(program)?;
+                            } else {
+                                println!("recieved unusual command: {}", msg.command)
+                            }
+                        }
+                        None => println!("recieved unusual person: {}", msg.sub),
+                    }
+                }
+                None => println!(
+                    "recieved unusual message. message should be <command> <sub>. got: {}",
+                    string
+                ),
+            }
+        }
+        fs::remove_file(&sock_path).unwrap_or_default();
     }
+
     Ok(())
 }
